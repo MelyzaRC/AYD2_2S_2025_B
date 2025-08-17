5163e02a1b875f142483195bb4b7730ab93a6efe
Melyza Alejandra Rodríguez Contreras <mlzdrg792@gmail.com>
2025-08-17 00:48:34 -0600

    clase 4

--- Clase3/ejemplo_clase3_patrones/src/decorator/decorator.java
+++ Clase3/ejemplo_clase3_patrones/src/decorator/decorator.java
@@ -2,12 +2,15 @@ package decorator;
 
 public class decorator {
 	public static void main(String[] args) {
+		
         Cafe cafe = new CafeSimple();
         System.out.println(cafe.descripcion()); 
         System.out.println("Costo: Q" + cafe.costo());
+        
         cafe = new ConLeche(cafe);
         System.out.println(cafe.descripcion()); 
         System.out.println("Costo: Q" + cafe.costo());
+        
         cafe = new ConPostre(cafe);
         System.out.println(cafe.descripcion()); 
         System.out.println("Costo: Q" + cafe.costo());
--- Clase3/ejemplo_clase3_patrones/src/state/state.java
+++ Clase3/ejemplo_clase3_patrones/src/state/state.java
@@ -3,7 +3,9 @@ package state;
 public class state {
 	public static void main(String[] args) {
 		ReproductorMusica reproductor = new ReproductorMusica();
-
+		
+		reproductor.pausar();
+		reproductor.detener();
 		reproductor.reproducir();
 		reproductor.pausar();
 		reproductor.reproducir();
